140fa9672b1501b047b4c9e1e6fd625a30da7d38
David Situ <davidsitu626@gmail.com>
2019-09-26 12:15:22 -0700

    Created Room Component with Helper Functions

--- src/components/World.js
+++ src/components/World.js
@@ -19,13 +19,48 @@ class World extends React.Component {
         cooldown: 15
       }
     };
-    this.move = this.move.bind(this);
   }
 
   componentDidMount() {
     this.start();
     // this.move();
   }
+
+    //Receive all elements of the Rooms
+
+  //Functions: 
+  // 1. Takes current object, return possible exits +
+  // 2. Vertices = Given room: and direction: next room
+  // 3. Get Room in the Direction
+  
+  //Save the information to a file or localstorage or api
+
+//make a room Class: in the state -> n: , S: , e: , w: , 
+//mapStatetoProps from World to Class -> 
+
+bfs =(starting_vertex) => {
+  //checks the direction key in the vertices
+  let stack = []
+  let visited = Set()
+  stack.push([starting_vertex])
+  while(stack.length > 0){
+    let path = stack.shift()
+    let node = path[path.length - 1]
+    if(!visited.has(node)){
+      if('visited[roomID] = ??'){
+        return path
+      } else{
+        visited.add(node)
+        for(let connection = 0; i < Object.values(vertices[node]).length; i++){
+          copy_path = [...path]
+          copy_path.push(connection)
+          stack.push(copy_path)
+        }
+      }
+    }
+  }
+
+}
 
   start = () => {
     // const token = localStorage.getItem("token");
@@ -52,6 +87,7 @@ class World extends React.Component {
       })
       .catch(err => {
         console.log("errors", err.response);
+        console.log("process", Config.appId);
       });
   };
 
@@ -90,48 +126,38 @@ class World extends React.Component {
       let currentRoom = this.state.currentRoom;
       console.log(`createMap, ${JSON.stringify(currentRoom)}`);
       const opDir = { n: "s", s: "n", e: "w", w: "e" };
-
       let traversalPath = [];
       let opPath = [];
 
-      if(Object.keys(mapGraph) !== currentRoom.roomID.toString()) {
-        console.log('mapgraph values', Object.keys(mapGraph))        
-        console.log('current room id', currentRoom.roomID.toString())
-        console.log('mapgraph current', mapGraph[currentRoom.roomID])
-
+      if (mapGraph[currentRoom.roomID] !== currentRoom.roomID) {
         mapGraph[currentRoom.roomID] = currentRoom;
-        console.log("opb4", opPath);
-        opPath.splice(opPath[opPath.length - 1]);
-        console.log("opafter", opPath[opPath.length - 1]);
+        localStorage.setItem("rooms", currentRoom);
 
-        //deadends have 1 exits
-        while (this.state.currentRoom.exits.length === 1) {
+        while (this.state.currentRoom.exits.length === 0) {
           let reverse = opPath.pop();
           traversalPath.push(reverse);
-          console.log('in the while')
           this.move(reverse);
         }
+
+        let x = localStorage.getItem("rooms");
+        console.log(JSON.stringify(x));
 
         let exits = this.state.currentRoom.exits;
         console.log("exits", exits);
 
         let move = exits.shift();
 
-        //pushing to traversal path but not using the traversal array
         traversalPath.push(move);
 
-        //pushing to opPath array but not using the array
         opPath.push(opDir[move]);
-        this.move(move);
 
         console.log("move dir", move);
-        console.log("cooldown", this.state.currentRoom.cooldown);
         console.log("roomID", this.state.currentRoom.roomID);
-        
+        this.move(move);
         console.log("move ran");
       }
 
-      console.log('out of the if',mapGraph);
+      console.log(mapGraph);
     };
     setInterval(map, 16000);
   };
